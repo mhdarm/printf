b7cce8fbeb1600f770faf4e3bd56e5f35d35ab32
mhdarm <mhdarmlry@yahoo.com>
2023-03-29 02:16:17 -0700

    printf update 0.02

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
+#include <limits.h>
+#include <stdio.h>
 #include "main.h"
-
 /**
  * main - Entry point
- *
+ *this project was done by Emmanuel and Bright
  * Return: Always 0
  */
 int main(void)
@@ -36,6 +37,7 @@ int main(void)
 	len2 = printf("Percent:[%%]\n");
 	_printf("Len:[%d]\n", len);
 	printf("Len:[%d]\n", len2);
-	_printf("Unknown:[%r]\n");
+	printf("Unknown:[%r]\n");
+	printf("Unknown:[%r]\n");
 	return (0);
 }
